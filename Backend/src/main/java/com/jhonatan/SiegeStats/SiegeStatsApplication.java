cde46356d1b82c5e248003b74dcfdba0c42930b2
vJhonatan <jhonatan99653@gmail.com>
2025-02-17 13:07:43 -0300

    initial commit

---
+++ Backend/src/main/java/com/jhonatan/SiegeStats/SiegeStatsApplication.java
@@ -0,0 +1,13 @@
+package com.jhonatan.SiegeStats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiegeStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiegeStatsApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/jhonatan/SiegeStats/SiegeStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhonatan.SiegeStats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiegeStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
